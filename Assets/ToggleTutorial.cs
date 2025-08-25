f53cf7d844568c07e8313f1ab169467ed2ddef8b
navisraditya <navisraditya@gmail.com>
2025-08-25 20:36:48 +0700

    objectives scene done

--- Assets/ToggleTutorial.cs
+++ Assets/ToggleTutorial.cs
@@ -1,26 +1,17 @@
 
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 namespace Perspective
 {
     public class ToggleTutorial : MonoBehaviour
     {
-        public CanvasGroup toOpen;
-        public CanvasGroup toClose;
+        public string toOpen;
 
         public void OnButtonClick()
         {
-            print("kepencet");
-            toOpen.alpha = 1f;
-            toOpen.interactable = true;
-            toOpen.blocksRaycasts = true;
-
-            if (toClose != null)
-            {
-                toClose.alpha = 0f;
-                toClose.interactable = false;
-                toClose.blocksRaycasts = false;
-            }
+            SceneManager.LoadScene(toOpen);
         }
     }
 }
